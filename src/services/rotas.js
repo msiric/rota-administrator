ba11925f6d362260344822eccd68412833448cf2
unknown <mario.siric@mail.com>
2020-12-08 14:54:32 +0100

    Switch http to https

--- src/services/rotas.js
+++ src/services/rotas.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
 export const getData = {
-  request: async () => await axios.get(`http://clava.io/api/rotas`),
+  request: async () => await axios.get(`https://clava.io/api/rotas`),
   success: { message: "Rotas successfully fetched", variant: "success" },
   error: { message: "Failed to fetch rotas", variant: "error" },
 };
 export const generateRota = {
-  request: async () => await axios.get(`http://clava.io/api/generate`),
+  request: async () => await axios.get(`https://clava.io/api/generate`),
   success: { message: "Rota successfully generated", variant: "success" },
   error: { message: "Failed to generate rota", variant: "error" },
 };
